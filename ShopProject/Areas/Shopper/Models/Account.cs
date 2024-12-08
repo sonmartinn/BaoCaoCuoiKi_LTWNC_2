112278440d1c8c6f32d496441c83a282f2acbb43
Oanh Le <leoanh19102003@gmail.com>
2024-12-09 04:36:20 +0700

    login and register (customer)

---
+++ ShopProject/Areas/Shopper/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopProject.Areas.Shopper.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
--- ShopProject/Areas/Shopper/Models/UserContext.cs
+++ ShopProject/Areas/Shopper/Models/UserContext.cs
@@ -12,6 +12,8 @@ namespace ShopProject.Areas.Shopper.Models
         {
         }
 
+        // DbSet cho các entity trong cơ sở dữ liệu
+        public virtual DbSet<Account> Accounts { get; set; }  // DbSet cho Account
         public virtual DbSet<Category> Categories { get; set; }
         public virtual DbSet<Comment> Comments { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
@@ -22,85 +24,100 @@ namespace ShopProject.Areas.Shopper.Models
         public virtual DbSet<ProductType> ProductTypes { get; set; }
         public virtual DbSet<Rate> Rates { get; set; }
 
+        // Hàm này được gọi khi EF tạo ra mô hình cơ sở dữ liệu từ lớp này
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Comment>()
-                .Property(e => e.proID)
-                .IsUnicode(false);
+            base.OnModelCreating(modelBuilder);
 
+            // Cấu hình cho các bảng
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Username)
+                .IsUnicode(false);  // Không lưu trữ Unicode cho Username
+
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Email)
+                .IsUnicode(false);  // Không lưu trữ Unicode cho Email
+
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Password)
+                .IsUnicode(false);  // Không lưu trữ Unicode cho Password
+
+
+
+            // Cấu hình cho các bảng khác (ví dụ: Customer, Order...)
             modelBuilder.Entity<Customer>()
                 .Property(e => e.cusPhone)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho phone
 
             modelBuilder.Entity<Customer>()
                 .Property(e => e.cusEmail)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho email
 
             modelBuilder.Entity<OrderDetail>()
                 .Property(e => e.orderID)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho orderID
 
             modelBuilder.Entity<OrderDetail>()
                 .Property(e => e.proID)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho product ID
 
             modelBuilder.Entity<OrderDetail>()
                 .Property(e => e.ordtsThanhTien)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho price
 
             modelBuilder.Entity<Order>()
                 .Property(e => e.orderID)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho order ID
 
             modelBuilder.Entity<Order>()
                 .Property(e => e.cusPhone)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho customer phone
 
             modelBuilder.Entity<Order>()
                 .Property(e => e.orderDateTime)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho datetime của đơn hàng
 
             modelBuilder.Entity<Order>()
                 .HasMany(e => e.OrderDetails)
                 .WithRequired(e => e.Order)
-                .WillCascadeOnDelete(false);
+                .WillCascadeOnDelete(false);  // Cấu hình mối quan hệ
 
             modelBuilder.Entity<Producer>()
                 .Property(e => e.pdcPhone)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho phone của Producer
 
             modelBuilder.Entity<Producer>()
                 .Property(e => e.pdcEmail)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho email của Producer
 
             modelBuilder.Entity<Product>()
                 .Property(e => e.proID)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho product ID
 
             modelBuilder.Entity<Product>()
                 .Property(e => e.proSize)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho size của sản phẩm
 
             modelBuilder.Entity<Product>()
                 .Property(e => e.proPrice)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho price của sản phẩm
 
             modelBuilder.Entity<Product>()
                 .Property(e => e.proUpdateDate)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho update date của sản phẩm
 
             modelBuilder.Entity<Product>()
                 .HasMany(e => e.OrderDetails)
                 .WithRequired(e => e.Product)
-                .WillCascadeOnDelete(false);
+                .WillCascadeOnDelete(false);  // Cấu hình mối quan hệ với OrderDetails
 
             modelBuilder.Entity<Product>()
                 .HasOptional(e => e.Rate)
-                .WithRequired(e => e.Product);
+                .WithRequired(e => e.Product);  // Cấu hình mối quan hệ 1-1 với Rate
 
             modelBuilder.Entity<Rate>()
                 .Property(e => e.proID)
-                .IsUnicode(false);
+                .IsUnicode(false);  // Không lưu trữ Unicode cho product ID trong bảng Rate
         }
     }
 }
